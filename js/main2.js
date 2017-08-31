ce50272a0a15757436543af96dbf0cbc0ddcf63d
fbecerra <becerrafernando@gmail.com>
2017-08-31 17:31:53 -0400

    Added sidebar

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,62 @@
 var scrollVis = function (evolution, description, stars, indices) {
 
+    /* Sidebar */
+
+    var setupSidebar = function () {
+        $(window).scroll(function () {
+            var window_top = $(window).scrollTop(); // the "12" should equal the margin-top value for nav.stick
+            var div_top = $('#graphic').offset().top;
+            if (window_top > div_top) {
+                $('nav').addClass('stick');
+            } else {
+                $('nav').removeClass('stick');
+            }
+        });
+
+        $("nav a").click(function (evn) {
+            evn.preventDefault();
+            console.log(this.hash)
+            $('html,body').scrollTo(this.hash, this.hash);
+        });
+
+        var aChildren = $("nav li").children(); // find the a children of the list items
+        var aArray = []; // create the empty aArray
+        for (var i = 0; i < aChildren.length; i++) {
+            var aChild = aChildren[i];
+            var ahref = $(aChild).attr('href');
+            aArray.push(ahref);
+        } // this for loop fills the aArray with attribute href values
+
+        $(window).scroll(function () {
+            var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+            var windowHeight = $(window).height(); // get the height of the window
+            var docHeight = $(document).height();
+
+            for (var i = 0; i < aArray.length; i++) {
+                var theID = aArray[i];
+                var divHeight = $(theID).height(); // get the height of the div in question
+                var divPos = $(theID).offset().top - divHeight/2; // get the offset of the div from the top of page
+
+
+                if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                    $("a[href='" + theID + "']").addClass("nav-active");
+                } else {
+                    $("a[href='" + theID + "']").removeClass("nav-active");
+                }
+            }
+
+            if (windowPos + windowHeight == docHeight) {
+                if (!$("nav li:last-child a").hasClass("nav-active")) {
+                    var navActiveCurrent = $(".nav-active").attr("href");
+                    $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+                    $("nav li:last-child a").addClass("nav-active");
+                }
+            }
+        });
+    };
+
+    setupSidebar();
+
     /* HR diagram definitions */
 
     var margin = {top: window.innerHeight/25, right: window.innerWidth/20, bottom: window.innerHeight/25, left: window.innerWidth/20},
@@ -342,6 +399,9 @@ var scrollVis = function (evolution, description, stars, indices) {
             bg_img.css("background-image", "url("+img+")")
                 .animate({opacity: 1}, transition_duration);
         }, transition_duration);
+
+        var span = $("#bg-img span");
+        img === 'img/bg.jpg' ? span.css("display", "block") : span.css("display", "none");
 
     };
 
